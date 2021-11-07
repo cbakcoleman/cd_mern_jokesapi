23dad79d49d4159aa204e76ede3c0ae03fa08e31
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-11-07 14:59:02 -0800

    create connection to mongodb

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,14 @@
+// 2. import mongoose (from node_modules, we installed it), require mongoose
+const mongoose = require("mongoose");
+const db_name = "jokesapi";
+
+
+// instead of hardcoding database name, can concatenate and declare db_name above
+// else mongoose.connect("mongodb://localhost/DBNAME", {......})
+mongoose.connect("mongodb://localhost/" + db_name, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    // Don't forget to use backticks for string interpolation
+    .then(() => console.log(`Established a connection to the ${db_name} database`))
+    .catch(error => console.log("Something went wrong when connecting to the database", error))
--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
+// 1. set up server using three lines below and app.listen(PORT......)
 const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// MIDDLEWARE
+app.use(express.json(), express.urlencoded({extended: true}))
+// This is the only server running in entire file (nodemon server.js), so if want to execute something from another file (see mongoose.config.js) then have to import that file into server.js
+// 3. connect to mongodb, require file below
+require("./config/mongoose.config");
+
+app.listen(PORT, () => console.log(`Server up on port: ${PORT}`))
